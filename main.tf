38043621a28db4600467c66bdb439a36347323f4
Umair Khan <m.umairkhan.co@proton.me>
2024-03-17 10:14:44 +0000

    Added aws terrafrom provider and created s3 bucket resource

--- main.tf
+++ main.tf
@@ -1,19 +1,33 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.16.2"
     }
   }
 }
 
+
 provider "random" {
-  # Configuration options
 }
 
+provider "aws" {
+}
+
+
 resource "random_string" "bucket_name" {
-  length   = 16
-  special  = false
+  length  = 16
+  special = false
+  lower = true
+  upper = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
