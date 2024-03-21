ab820bc8a6cb3f11290925ea7389b14fcde7a8e8
Umair Khan <m.umairkhan.co@proton.me>
2024-03-21 11:27:15 +0000

    FIX terraform login script

--- bin/terraform-login.sh
+++ bin/terraform-login.sh
@@ -3,7 +3,24 @@
 # This script logins to terraform cloud
 
 
-cat > /home/$USER/.terraform.d/credentials.tfrc.json << EOF
+
+# Define target directory and file
+TARGET_DIR="/home/$USER/.terraform.d"
+TARGET_FILE="${TARGET_DIR}/credentials.tfrc.json"
+
+# Check if TERRAFORM_CLOUD_TOKEN is set
+if [ -z "$TERRAFORM_CLOUD_TOKEN" ]; then
+    echo "Error: TERRAFORM_CLOUD_TOKEN environment variable is not set."
+    exit 1
+fi
+
+# Check if directory exists, if not, create it
+if [ ! -d "$TARGET_DIR" ]; then
+    mkdir -p "$TARGET_DIR"
+fi
+
+# Generate credentials.tfrc.json with the token
+cat > "$TARGET_FILE" << EOF
 {
   "credentials": {
     "app.terraform.io": {
@@ -12,3 +29,6 @@ cat > /home/$USER/.terraform.d/credentials.tfrc.json << EOF
   }
 }
 EOF
+
+echo "${TARGET_FILE} has been generated."
+
