f43c59e3a5e03327f16b984018b109b05aa48a7d
Umair Khan <m.umairkhan.co@proton.me>
2024-03-22 22:47:10 +0000

    Added module tf files

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-terraform {
-  cloud {
-    organization = "i-umairkhan"
-    workspaces {
-      name = "terra-town"
-    }
-  }
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-}
-
-
-provider "random" {}
-
-provider "aws" {}
-
-
 resource "random_string" "bucket_name" {
   length  = 16
   special = false
@@ -32,8 +7,7 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  # tags = {
+  #   UserUuid = var.user_uuid
+  # }
 }
