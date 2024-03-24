b7fa3d0ccc46a40c1270443ae29ec12400df8339
Umair Khan <m.umairkhan.co@proton.me>
2024-03-24 18:53:17 +0000

    Moved s3 into terrrahouse module

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-  tags = {
-    UserUuid = var.user_uuid
-  }
+terraform {
+  
+} 
+
+# resource "aws_s3_bucket" "website_bucket" {
+# }
+
+module "terrahouse-aws" {
+  source = "./modules/terrahouse-aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
