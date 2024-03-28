96e7ae5359de7fa99ab0671a9a4e3873e2173e8f
Umair Khan <m.umairkhan.co@proton.me>
2024-03-28 02:23:42 +0000

    Invalidate cloud front cache on content version update

--- modules/terrahouse-aws/resource-cdn.tf
+++ modules/terrahouse-aws/resource-cdn.tf
@@ -56,3 +56,15 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = true
   }
 }
+
+
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+  provisioner "local-exec" {
+    command = <<COMMAND
+aws cloudfront create-invalidation \
+--distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+--paths '/*'
+    COMMAND
+  }
+}
